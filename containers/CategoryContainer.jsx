b48fcb3fae76275fb4b8aa1098fb487d71a14a72
furkan <132236699+furkantaglikk@users.noreply.github.com>
2023-09-09 00:24:31 +0300

    Eslint Desteği Eklendi

---
+++ containers/CategoryContainer.jsx
@@ -0,0 +1,14 @@
+import Categories from '@/components/Categories'
+import Section from '@/components/Section'
+
+const CategoryContainer = ({ categoryName, sectionTitle }) => {
+    return (
+        <div>
+            <Categories />
+            <Section sectionTitle={sectionTitle} movies={categoryName} />
+
+        </div>
+    )
+}
+
+export default CategoryContainer
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['image.tmdb.org', 'm.media-amazon.com'],
+    },
+}
 
 module.exports = nextConfig
